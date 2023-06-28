4149efff7893ad82698879e01e7f88d3c1573484
reabetsweModise <reabetswemodise02@gmail.com>
2023-06-28 11:59:11 -0700

    flags

--- get_flags.c
+++ get_flags.c
@@ -10,8 +10,6 @@
  */
 int get_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
 	int loop_counter, current_index;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
